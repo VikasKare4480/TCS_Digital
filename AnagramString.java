7d8491d8467101bed4094f764c5aae7e66be6513
VikasKare <karevikas7777@gmail.com>
2023-12-28 23:24:47 +0530

    AnagramString

--- AnagramString.java
+++ AnagramString.java
@@ -1,49 +1,23 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 class AnagramString {
     
     boolean isAnagram(String original, String anagramCheck) { 
 
-        // char[] origin = original.toLowerCase().replace("//s", "").toCharArray();
-
-        original = original.toLowerCase().replace("\\s", "");
-        anagramCheck = anagramCheck.toLowerCase().replace("\\s", "");
-        
         if(original.length() != anagramCheck.length()) {
 
             return false;
-
-        }else {
-
-            for(int i = 0; i < original.length(); i++) {
-
-                char ch = original.charAt(i);
-                int oriCount = 0;
-
-                for(int j = 0; j < original.length(); j++) {
-
-                    if(ch == original.charAt(j)) {
-
-                        oriCount++;
-                    }
-                }
-
-                int anaCount = 0;
-                for(int j = 0; j < anagramCheck.length(); j++) {
-
-                    if(ch == anagramCheck.charAt(j)) {
-
-                        anaCount++;
-                    }
-                }
-
-                if(anaCount != oriCount) {
-
-                    return false;
-                }
-            }
         }
-        return true;
+        original = original.toLowerCase().replace("\\s", "");
+        anagramCheck = anagramCheck.toLowerCase().replace("\\s", "");
+
+        char[] orginArr = original.toCharArray();
+        char[] anagramArr = anagramCheck.toCharArray();
+
+        Arrays.sort(orginArr);
+        Arrays.sort(anagramArr);
+        
+        return Arrays.equals(orginArr, anagramArr);
     }
     public static void main(String[] args) {
 
